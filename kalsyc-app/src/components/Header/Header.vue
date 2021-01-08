7eaddfe85a6285e5b269534068de160c31783a76
Darren Sim <klassiclegend@gmail.com>
2021-01-08 23:13:10 +0800

    Redo header style with mobile design

--- kalsyc-app/src/components/Header/Header.vue
+++ kalsyc-app/src/components/Header/Header.vue
@@ -10,6 +10,14 @@
     sendEmail(): void {
       window.location.assign("mailto:kalsycofficial@gmail.com");
     }
-}
+
+    toggleMenu(): void {
+      const element: HTMLElement | null = document.getElementById("nav-toggle");
+      if (element) {
+        element.classList.toggle("nav-toggle");
+      }
+    }
+
+  }
 
 </script>

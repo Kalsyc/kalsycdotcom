8c4a602706275814cd95fefd53c0871b9c40a72b
Darren Sim <klassiclegend@gmail.com>
2021-01-11 01:36:35 +0800

    'deploy'

---
+++ precache-manifest.b35b577db88934f55db1e0389d8693ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c361eafdf5bfda5a0963",
+    "url": "/kalsycdotcom/css/app.cb1400ce.css"
+  },
+  {
+    "revision": "47e90048fdf22518c6641d994cc22ded",
+    "url": "/kalsycdotcom/img/darren.47e90048.jpg"
+  },
+  {
+    "revision": "77529d5fb6f213c351d20126fdf9db49",
+    "url": "/kalsycdotcom/img/menu-btn.77529d5f.svg"
+  },
+  {
+    "revision": "ba2ff4c3e601b900cfb6301168c15a15",
+    "url": "/kalsycdotcom/index.html"
+  },
+  {
+    "revision": "c361eafdf5bfda5a0963",
+    "url": "/kalsycdotcom/js/app.82fc236c.js"
+  },
+  {
+    "revision": "0df33948198788173cf7",
+    "url": "/kalsycdotcom/js/chunk-vendors.b7ba3d14.js"
+  },
+  {
+    "revision": "b4b606a8694286a2378c9a677e78f75f",
+    "url": "/kalsycdotcom/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kalsycdotcom/robots.txt"
+  }
+]);
